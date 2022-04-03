aff43c01dccdde2ea19019bd53f4410daf84196d
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2022-04-03 12:47:57 +0200

    This is the master branch version of the image carousel mtn component first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-carousel-mtn';
+
+  serviceArray1:any=[
+    {
+      imageName: "../assets/mtn-images/data-mobile.svg"
+    },
+  ]
+
+  serviceArray2:any=[
+    {  imageName:"../assets/mtn-images/internet.svg" },
+  ]
+    serviceArray3:any=[
+    {
+      imageName: "../assets/mtn-images/devices.svg"
+    },
+  ]
+  serviceArray4:any=[{
+    imageName: "../assets/mtn-images/plans.svg"
+
+  }
+  ]
+  serviceArray5:any=[
+    {
+      imageName: "../assets/mtn-images/upgrade.svg"
+    },
+  ]
+
+  serviceOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      // 940: {
+      //   items: 4
+      // }
+    },
+    nav: true
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CarouselModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
